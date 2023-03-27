cb0929eb278eafec077c9d70a7800a715b0a0645
beemusy <beemusy@gmail.com>
2023-03-27 07:45:50 -0700

    1-printf.c

--- 1-printf.c
+++ 1-printf.c
@@ -41,3 +41,43 @@ int op_i(va_list n)
 	}
 	return (len);
 }
+
+
+/**
+ *rangeof - Recognize a value to print.
+ *@n: Number to convert.
+ *Return: An integer.
+ */
+int rangeof(int n)
+{
+	int number = n;
+	char digit, negative = '-';
+
+	if (number < 0)
+	{
+		number = -number;
+		write(1, &negative, 1);
+	}
+
+	if (number / 10 > 0)
+		rangeof(number / 10);
+	digit = (number % 10) + '0';
+	write(1, &digit, 1);
+	return (number);
+}
+
+/**
+ *op_d - Prints a integer.
+ *@d: Character to integer.
+ *Return: An integer.
+ */
+
+int op_d(va_list d)
+
+{
+	int a;
+
+	a = va_arg(d, int);
+	rangeof(a);
+	return (0);
+}
