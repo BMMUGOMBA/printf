22520e2f73b229c127e34e24b8c260c37452442d
beemusy <beemusy@gmail.com>
2023-03-27 07:17:59 -0700

    main.h

--- main.h
+++ main.h
@@ -16,3 +16,17 @@ typedef struct formats
 	char *f;
 	int (*func)(va_list);
 } forma_t;
+
+int _printf(const char *format, ...);
+int _putchar(char c);
+int format_h(const char *format, forma_t *stc_ar, va_list arg);
+
+/* Single prototypes as calc task */
+int op_c(va_list c);
+int op_s(va_list s);
+int rangeof(int n);
+int rangeofInt(int n);
+int op_d(va_list d);
+int op_i(va_list n);
+int powten(int n);
+#endif /* _MAIN_H_ */
