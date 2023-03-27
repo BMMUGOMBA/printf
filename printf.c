19ab41cfc05762f0adc00bcb4279e2fc92f77137
beemusy <beemusy@gmail.com>
2023-03-27 07:30:35 -0700

    printf.c function

--- printf.c
+++ printf.c
@@ -1,1 +1,7 @@
-
+#include "main.h"
+/**
+ * _printf - Function that sends formatted output to stdout.
+ * @format: Character string. Is composed of zero or more directives.
+ * Return: Print a new line at the end of your function.
+ */
+int _printf(const char *format, ...)
