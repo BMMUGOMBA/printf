3c92dffd316c88fea6afee7c92d13e6a61e5f18f
BMMUGOMBA <bmmugomba@gmail.com>
2023-03-27 16:15:37 +0200

    batsi git commit first main

--- main.h
+++ main.h
@@ -1,1 +1,18 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * struct formats - structre to handle the different formats
+ * @f: the format
+ * @func: the function pointer
+ */
+typedef struct formats
+{
+	char *f;
+	int (*func)(va_list);
+} forma_t;
